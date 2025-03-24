c0bb376b04a493b7836a45a4b7b9990a232d1657
sam sang <linming1007@gmail.com>
2025-03-24 11:28:32 +0800

    add support for app shortcuts

--- android/app/src/main/kotlin/com/example/fymemos/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/fymemos/MainActivity.kt
@@ -1,5 +1,27 @@
 package com.example.fymemos
 
 import io.flutter.embedding.android.FlutterActivity
+import android.content.Intent
+import android.os.Bundle
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        handleShortcut(intent)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleShortcut(intent)
+    }
+
+    private fun handleShortcut(intent: Intent) {
+        if (intent.action == "android.intent.action.VIEW") {
+            val path = intent.data?.path
+            if (path == "/create_memo") {
+                val flutterEngine = FlutterEngine(this)
+                flutterEngine.navigationChannel.setInitialRoute("/create_memo")
+            }
+        }
+    }
+}
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,32 @@ import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
+  private var methodChannel: FlutterMethodChannel?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    // 初始化 MethodChannel
+    guard let controller = window?.rootViewController as? FlutterViewController else {
+        fatalError("rootViewController is not type FlutterViewController")
+    }
+    methodChannel = FlutterMethodChannel(
+        name: "app_shortcuts",
+        binaryMessenger: controller.binaryMessenger
+    )
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  override func application(
+        _ application: UIApplication,
+        performActionFor shortcutItem: UIApplicationShortcutItem,
+        completionHandler: @escaping (Bool) -> Void
+    ) {
+        if shortcutItem.type == "CreateMemoShortcut" {
+            methodChannel?.invokeMethod("launchFromShortcut", arguments: "/create_memo")
+        }
+        completionHandler(true)
+    }
 }
