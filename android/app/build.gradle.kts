04ca123223b9326b32bd334a10e39811c8e4e568
sam sang <linming1007@gmail.com>
2025-03-25 13:26:22 +0800

    fix: compile error in android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,9 +41,9 @@ android {
         create("release") {
             if (System.getenv("GITHUB_ACTIONS") == "true") {
                 storeFile = file("keystore.jks")
-                storePassword = System.getenv["STORE_PASSWORD"] as String?
-                keyAlias = System.getenv["KEY_ALIAS"] as String?
-                keyPassword = System.getenv["KEY_PASSWORD"] as String?
+                storePassword = System.getenv("STORE_PASSWORD")
+                keyAlias = System.getenv("KEY_ALIAS")
+                keyPassword = System.getenv("KEY_PASSWORD")
             }
         }
     }
